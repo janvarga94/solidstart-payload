21ebcd4065093a0a19c14ed59d432429199d4d92
Jan <janvarga94@gmail.com>
2025-09-29 15:33:09 +0200

    ok

--- packages/website/src/routes/index.tsx
+++ packages/website/src/routes/index.tsx
@@ -3,28 +3,36 @@ import Counter from "~/components/Counter";
 import { getPayload } from "payload";
 import { createAsync } from "@solidjs/router";
 import { config } from "@shared-models";
+import { Show } from "solid-js";
 
 const getData = async () => {
-    "use server";
-    const payloadConfig = await config;
-    const payload = await getPayload({ config: payloadConfig });
-    return 3;
+  "use server";
+  const payloadConfig = await config;
+  const payload = await getPayload({ config: payloadConfig });
+  return await payload.find({ collection: "posts", limit: 100 });
 };
 
 export default function Home() {
-    let asyncstuff = createAsync(getData);
-    return (
-        <main>
-            <Title>Hello World</Title>
-            <Counter />
-            <h1>Hello world! 1Jan {asyncstuff()}</h1>
-            <p>
-                Visit{" "}
-                <a href="https://start.solidjs.com" target="_blank">
-                    start.solidjs.com
-                </a>{" "}
-                to learn how to build SolidStart apps.
-            </p>
-        </main>
-    );
+  let posts = createAsync(getData);
+  return (
+    <main>
+      <Title>Hello World</Title>
+      <h1>Hello world!</h1>
+      <Counter />
+      <Show when={posts()}>
+        {(posts) => (
+          <p>
+            There are {posts().docs.length} posts in the Payload CMS database.
+          </p>
+        )}
+      </Show>
+      <p>
+        Visit{" "}
+        <a href="https://start.solidjs.com" target="_blank">
+          start.solidjs.com
+        </a>{" "}
+        to learn how to build SolidStart apps.
+      </p>
+    </main>
+  );
 }
